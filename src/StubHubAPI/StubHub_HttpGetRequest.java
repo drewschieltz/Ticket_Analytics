ae8f9847abf588b6ee4c13d02556376a1d1df898
ASchieltz <drewschieltz@gmail.com>
2017-06-21 20:16:11 -0400

    1 - Moved sendGetRequest to superclass

--- src/StubHubAPI/StubHub_HttpGetRequest.java
+++ src/StubHubAPI/StubHub_HttpGetRequest.java
@@ -1,28 +1,96 @@
 package StubHubAPI;
 
-import StubHubAPI.AccountManagementAPI.GET.*;
-import StubHubAPI.EventsAPI.GET.*;
-import StubHubAPI.EventSearchAPI.GET.*;
-import StubHubAPI.InventorySearchAPI.GET.*;
-import StubHubAPI.ListingsAPI.GET.*;
-import StubHubAPI.UserManagementAPI.GET.*;
+//import StubHubAPI.AccountManagementAPI.GET.*;
+//import StubHubAPI.EventsAPI.GET.*;
+//import StubHubAPI.EventSearchAPI.GET.*;
+//import StubHubAPI.InventorySearchAPI.GET.*;
+//import StubHubAPI.ListingsAPI.GET.*;
+//import StubHubAPI.UserManagementAPI.GET.*;
 import StubHubAPI.VenuesAPI.GET.*;
-import com.mongodb.*;
-import org.json.JSONObject;
 
+import com.mongodb.*;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.params.ClientPNames;
+import org.apache.http.client.params.CookiePolicy;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+import org.json.XML;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Set;
 
 public class StubHub_HttpGetRequest {
 
     //Fields
-    public final String USER_AGENT = "Mozilla/5.0";
     public final String token = "16ede3a5-ad0f-3042-991a-adb3e2e2754a";
 
 
     //Run code - For testing only
     public static void main(String[] args) throws Exception {
         getVenueInfo("180239");
+    }
+
+
+    /*******************************************************
+     * Send Get Request
+     ******************************************************/
+
+    public void sendGetRequest(String path, String collectionName) {
+
+        System.out.println("Sending Http GET request......");
+
+        try {
+
+            //Set up client & request
+            HttpClient client = new DefaultHttpClient();
+            client.getParams().setParameter(ClientPNames.COOKIE_POLICY, CookiePolicy.IGNORE_COOKIES);
+            HttpGet request = new HttpGet(path);
+
+            // add request headers
+            String token = "16ede3a5-ad0f-3042-991a-adb3e2e2754a";
+            request.addHeader("Authorization", "Bearer " + token);
+            request.addHeader("User-Agent", "Mozilla/5.0");
+
+            //Execute request
+            HttpResponse response = client.execute(request);
+
+            System.out.println("\nSending 'GET' request to URL : " + path);
+            int respCode = response.getStatusLine().getStatusCode();
+            System.out.println("Response Code : " + respCode);
+            System.out.println("-------------");
+
+            if (respCode == 200) {
+                //Load response result
+                BufferedReader rd = new BufferedReader(
+                        new InputStreamReader(response.getEntity().getContent()));
+
+                StringBuilder result = new StringBuilder();
+                String line = "";
+                while ((line = rd.readLine()) != null) {
+                    result.append(line);
+                }
+
+                //Load data into db
+                System.out.println();
+                System.out.println("Pushing info into the database......");
+                System.out.println();
+
+                JSONObject xmlJSONObj = XML.toJSONObject(result.toString());
+                int code = loadIntoDB(xmlJSONObj, collectionName);
+
+                System.out.println();
+                System.out.println("Mongo Response Code: " + code);
+            } else {
+                System.out.println("Data retrieval unsuccessful!");
+            }
+        } catch (Exception e) {
+            System.out.println("Error retrieving venue information");
+            e.printStackTrace();
+        }
     }
 
 
@@ -40,7 +108,7 @@ public class StubHub_HttpGetRequest {
     *   300 - Duplicate
     *   400 - Unknown error
     */
-    public int loadIntoDB(JSONObject json, String collectionName) {
+    private int loadIntoDB(JSONObject json, String collectionName) {
         try {
             // Initialize variables
             System.out.println("Connecting to database.....");
@@ -104,7 +172,6 @@ public class StubHub_HttpGetRequest {
         List<String> names = mongo.getDatabaseNames();
 
         for (final String name : names) {
-            System.out.println(name);
             if (name.equals(dbName)) {
                 return false;
             }
@@ -241,8 +308,8 @@ public class StubHub_HttpGetRequest {
     /*
      * Get venue information.
      */
-    public static void getVenueInfo(String venueID) {
+    private static void getVenueInfo(String venueID) {
         Venue_Info http = new Venue_Info();
-        http.sendGetRequest(venueID);
+        http.getRequestData(venueID);
     }
 }
--- src/StubHubAPI/VenuesAPI/GET/Venue_Info.java
+++ src/StubHubAPI/VenuesAPI/GET/Venue_Info.java
@@ -19,58 +19,7 @@ import java.io.InputStreamReader;
 public class Venue_Info extends StubHub_HttpGetRequest {
 
     // HTTP GET request
-    public void sendGetRequest(String venueID) {
-
-        System.out.println("Sending Http GET request......");
-
-        try {
-            //Access path
-            String url = "https://api.stubhub.com/catalog/venues/v2/" + venueID;
-
-            //Set up client & request
-            HttpClient client = new DefaultHttpClient();
-            client.getParams().setParameter(ClientPNames.COOKIE_POLICY, CookiePolicy.IGNORE_COOKIES);
-            HttpGet request = new HttpGet(url);
-
-            // add request headers
-            String token = "16ede3a5-ad0f-3042-991a-adb3e2e2754a";
-            request.addHeader("Authorization", "Bearer " + token);
-            request.addHeader("User-Agent", USER_AGENT);
-
-            //Execute request
-            HttpResponse response = client.execute(request);
-
-            System.out.println("\nSending 'GET' request to URL : " + url);
-            int respCode = response.getStatusLine().getStatusCode();
-            System.out.println("Response Code : " + respCode);
-            System.out.println("-------------");
-
-            if (respCode == 200) {
-                //Load response result
-                BufferedReader rd = new BufferedReader(
-                        new InputStreamReader(response.getEntity().getContent()));
-
-                StringBuffer result = new StringBuffer();
-                String line = "";
-                while ((line = rd.readLine()) != null) {
-                    result.append(line);
-                }
-
-                //Load data into db
-                System.out.println();
-                System.out.println("Pushing info into the database......");
-
-                JSONObject xmlJSONObj = XML.toJSONObject(result.toString());
-                int code = loadIntoDB(xmlJSONObj, "Venues");
-
-                System.out.println();
-                System.out.println("Mongo Response Code: " + code);
-            } else {
-                System.out.println("Data retrieval unsuccessful!");
-            }
-        } catch (Exception e) {
-            System.out.println("Error retrieving venue information");
-            e.printStackTrace();
-        }
+    public void getRequestData(String venueID) {
+        sendGetRequest("https://api.stubhub.com/catalog/venues/v2/" + venueID, "Venues");
     }
 }
